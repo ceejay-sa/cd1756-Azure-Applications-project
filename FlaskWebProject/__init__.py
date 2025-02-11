4b8c37616551a37694521f922a2900df3f60e5b2
ceejay-sa <ceejay.sahagun@chevron.com>
2025-02-12 00:34:42 +0800

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -5,7 +5,7 @@ import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, user_logged_in, user_login_failed
+from flask_login import LoginManager
 from flask_session import Session
 
 app = Flask(__name__)
@@ -20,15 +20,5 @@ Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
-
-# Log successful login attempts
-@user_logged_in.connect_via(app)
-def log_successful_login(sender, user):
-    app.logger.info(f"User {user.username} logged in successfully.")
-
-# Log invalid login attempts
-@user_login_failed.connect_via(app)
-def log_invalid_login(sender, user):
-    app.logger.warning("Invalid login attempt.")
 
 import FlaskWebProject.views
